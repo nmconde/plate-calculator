bbf29a1f726689214f4c28d5db9a78ad47d4a961
Nicholas Conde <56334226+nmconde@users.noreply.github.com>
2019-10-16 22:00:30 -0400

    Update PlateCalculator.py
    Adjusted the rounding for the adjusted weights in the plate calculator function to be round to the closest rather than using math.floor

--- PlateCalculator.py
+++ PlateCalculator.py
@@ -3,12 +3,6 @@ import math
 import csv
 # Add your script below this line
 
-# User Inputs
-testweight = 230.7*0.9;
-barweight = 45;
-weights = [2.5,5,10,25,45,35];
-percentages = [70,80,90,70]
-
 """
 Function Name: Plate Calculator
 Function Application: Takes the testweight, barweight, and assorted weights and finds optimal barbell loading
@@ -18,7 +12,7 @@ def platecalculator(testweight,barweight,weights):
     processed_weights = sorted(weights,reverse=True);
     num_weights = len(weights);
     halfweight1 = (testweight-barweight)/2;
-    halfweight2 = math.floor(halfweight1/(min(processed_weights)));
+    halfweight2 = round(halfweight1/(min(processed_weights)));
     runweight = halfweight2*(min(processed_weights));
     adjustedweight = (runweight*2)+barweight;
 
@@ -58,19 +52,29 @@ def percentagecalculator(testweight,barweight,weights,percentages):
         plates_row.append([processed_weights[z]])
     for x in range(0,len(percentages)):
         PerAdjusted, ProcessedWeights, PerFinalNum = platecalculator((testweight*(percentages[x]/100)),barweight,weights);
-        percentagerow = [percentages[x],maxweight*(percentages[x]/100),PerAdjusted];
+        percentagerow = [percentages[x],testweight*(percentages[x]/100),PerAdjusted];
         for j in range(0,len(processed_weights)):
             percentagerow.append(PerFinalNum[j])
         csv_data.append(percentagerow)
     return maxweight, csv_data, plates_row;
 
-adjustedweight, processed_weights, final_num = platecalculator(testweight,barweight,weights)
-maxweight, csv_data, plates_row = percentagecalculator(testweight,barweight,weights,percentages)
+# User Inputs
+name = ["Carey"]
+exercises = ["Bench","Squat"];
+exercise_weight = [81.8*0.9,110.3*0.9];
+barweight = 45;
+weights = [2.5,5,10,25,45,35];
+percentages = [75,85,95,75]
 
 with open('531_weights.csv','w') as f:
     writer = csv.writer(f, lineterminator = '\n')
-    writer.writerow(["Plate Calculator"])
-    writer.writerow(["Training Max:",testweight])
-    writer.writerow(["Adjusted Max:",maxweight])
-    writer.writerow(plates_row)
-    writer.writerows(csv_data)
+    writer.writerow(["Plate Calculator:",name])
+    for x in (0,len(exercises)-1):
+        adjustedweight, processed_weights, final_num = platecalculator(exercise_weight[x],barweight,weights)
+        maxweight, csv_data, plates_row = percentagecalculator(exercise_weight[x],barweight,weights,percentages)
+        writer.writerow([])
+        writer.writerow([exercises[x]])
+        writer.writerow(["Training Max:",exercise_weight[x]])
+        writer.writerow(["Adjusted Max:",maxweight])
+        writer.writerow(plates_row)
+        writer.writerows(csv_data)
